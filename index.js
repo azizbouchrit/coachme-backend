699c06f60dcb9103e381089b1d71ef038e94c320
azizbouchrit <medazizbouchrit@gmail.com>
2021-05-29 11:46:14 +0200

    cors fix

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const mongoose = require("mongoose");
 var cors = require('cors')
 
@@ -37,7 +36,6 @@ mongoose
   .catch((err) => console.error("could not connect to mongodb ...", err));
 app.use(cors());
 app.use(express.json());
-app.use(cors())
 
 
 app.use("/api/goals", goals);
